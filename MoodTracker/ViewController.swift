fe95372521a9515a21cf3f2ca23e87aad8cf4fdc
Илья Лебедев <i.lebedev@bestdoctor.ru>
2019-08-11 13:25:29 +0300

    Initial Commit

---
+++ MoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by Ilya Lebedev on 11/08/2019.
+//  Copyright © 2019 Ilya Lebedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
